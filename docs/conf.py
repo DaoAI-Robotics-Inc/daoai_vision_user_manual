ef498084ffcd69c04254b46c06f9c2356faaa6b2
Duo Xu <dxu@daoai.com>
2023-11-09 22:19:17 +0000

    Merged in multi-version-support (pull request #172)
    Multi version support
    
    * Add sphinx-multiversion extension
    
    * Branch filter
    
    * Update regex pattern
    
    * Override versions.html from the Read the Docs theme
    
    * Add version banner if the current version is not the latest
    
    * Highlight banner
    
    * Update regex pattern
    
    
    Approved-by: Xiaochuan Chen

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ author = 'DaoAI'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_multiversion",
     "sphinx_inline_tabs",
     "sphinxcontrib.video",
     "sphinx.ext.autosectionlabel",
@@ -60,6 +61,8 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+smv_branch_whitelist = r'^.*$'
 
 def setup(app):
     app.add_css_file('my_theme.css')
